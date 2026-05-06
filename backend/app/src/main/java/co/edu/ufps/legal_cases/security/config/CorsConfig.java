14ff62e8555e0d087daf1b086da4bb5222df9fb6
José Manuel Pérez Rodríguez <anorakteam@gmail.com>
2026-05-05 21:52:53 -0500

    agregar configuración de CORS para permitir patrones de origenes permitidos

--- backend/app/src/main/java/co/edu/ufps/legal_cases/security/config/CorsConfig.java
+++ backend/app/src/main/java/co/edu/ufps/legal_cases/security/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOriginPatterns("http://localhost:3000", "https://*.vercel.app", "https://sistema-casos-juridicos.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
